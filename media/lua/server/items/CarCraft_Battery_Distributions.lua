54be6958e24868b2ab5457de03afac3a32cc42a0
Victor Espinoza <thesilvervelvets@gmail.com>
2022-01-10 15:05:52 +0000

    Uploading Mod
    Addings all files for debugging

---
+++ media/lua/server/items/CarCraft_Battery_Distributions.lua
@@ -0,0 +1,146 @@
+-- Referenced from More VHS, check them out
+-- https://steamcommunity.com/sharedfiles/filedetails/?id=2671117800
+
+--##################################################################################
+-- Item Reference Tables
+--##################################################################################
+
+-- Distro is set to 100 instead of 1 BETTER CHANCE TO FIND IT!
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "Carcraft.CarBatteryS4");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "Carcraft.CarBatteryS4");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "Carcraft.CarBatteryS4");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "Carcraft.CarBatteryS4");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "Carcraft.CarBatteryS4");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 100);
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "Carcraft.CarBatteryS5");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "Carcraft.CarBatteryS5");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "Carcraft.CarBatteryS5");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "Carcraft.CarBatteryS5");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "Carcraft.CarBatteryS5");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 100);
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "Carcraft.CarBatteryS6");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "Carcraft.CarBatteryS6");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "Carcraft.CarBatteryS6");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "Carcraft.CarBatteryS6");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 100);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "Carcraft.CarBatteryS6");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 100);
+
+
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "RearWindshield1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "RearWindshield1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "RearWindshield1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "RearWindshield1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "RearWindshield1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "RearWindshield2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "RearWindshield2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "RearWindshield2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "RearWindshield2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "RearWindshield2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "RearWindshield3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "RearWindshield3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "RearWindshield3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "RearWindshield3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "RearWindshield3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
+
+
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "FrontWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "FrontWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "FrontWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "FrontWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "FrontWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "FrontWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "FrontWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "FrontWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "FrontWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "FrontWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "FrontWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "FrontWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "FrontWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "FrontWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "FrontWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
+
+
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "RearWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "RearWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "RearWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "RearWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "RearWindow1");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "RearWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "RearWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "RearWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "RearWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "RearWindow2");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
+
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, "RearWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfTools"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, "RearWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfWheels"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, "RearWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfOutfit"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, "RearWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfSuspension"].items, 1);
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, "RearWindow3");
+table.insert(ProceduralDistributions["list"]["MechanicShelfBooks"].items, 1);
